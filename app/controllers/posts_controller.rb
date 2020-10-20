f3921f28f08f67eed33381c15f9dee776f099fdb
kou829 <73116796+kou829@users.noreply.github.com>
2020-10-20 11:25:32 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+
+  def index 
+    @posts = Post.all # 全てのレコードを@postsに代入
+  end
+  
+  def new
+  end
+
+  def create
+  end
+
+  def create
+    Post.create(content: params[:content])
+  end
+end
